d218d72c40ad2152c25b0b62e24cd91b633dc08c
Jeremy <jeremy@theknights.me>
2020-08-19 23:41:14 -0500

    initial commit

---
+++ BlazorWasmHostedSample/Client/Components/CodeInInheritedClassComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace BlazorWasmHostedSample.Client.Components
+{
+    public class CodeInInheritedClassComponent : ComponentBase
+    {
+        public bool IsLoading { get; set; } = true;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Delay(5000);
+            this.IsLoading = false;
+            await base.OnInitializedAsync(); // Important to keep 'base.' calls in Blazor. OnParameterSet is a must.
+        }
+    }
+}
---
+++ BlazorWasmHostedSample/Client/Components/CodeInPartialClass.razor.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace BlazorWasmHostedSample.Client.Components
+{
+    public partial class CodeInPartialClass
+    {
+        private bool isLoading = true;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Delay(5000);
+            this.isLoading = false;
+            await base.OnInitializedAsync(); // Important to keep 'base.' calls in Blazor. OnParameterSet is a must.
+        }
+    }
+}
